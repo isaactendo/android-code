307d6d84fb0181eb8765a653fb706c89cee34927
isaac <silaisaac71@gmail.com>
2025-06-04 12:15:03 +0300

    android code

---
+++ app/serializer.py
@@ -0,0 +1,10 @@
+#Serializer
+from rest_framework import serializers
+
+from app.models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
---
+++ app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from app.models import Student
+from app.serializer import StudentSerializer
+
+
+# Create your view
+#
+# s here.
+@api_view(['POST'])
+def save(request):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#ngrok
+@api_view(['GET'])
+def fetch(request):
+    students = Student.objects.create(name ="Joel Katana", email="joe@gmail.com", password="122345", gender="male", sport="soccer", education="college")
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data)
